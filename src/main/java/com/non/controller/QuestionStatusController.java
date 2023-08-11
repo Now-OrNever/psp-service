ba780611eab14b1dae32d35126bc9299a41420c8
Umang <umanggg100@gmail.com>
2023-08-11 15:15:14 +0530

    make the delete API for questionStatus

--- src/main/java/com/non/controller/QuestionStatusController.java
+++ src/main/java/com/non/controller/QuestionStatusController.java
@@ -3,10 +3,14 @@ package com.non.controller;
 
 import com.non.exception.QuestionNotFoundException;
 import com.non.exception.QuestionStatusNotFoundException;
+import com.non.model.Question;
 import com.non.model.QuestionStatus;
 import com.non.repository.QuestionStatusRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/non/questionstatus")
@@ -20,9 +24,9 @@ public class QuestionStatusController {
     }
 
     @GetMapping("/{questionId}/{userId}")
-    public QuestionStatus findQuestionStatus(@PathVariable String questionId, @PathVariable String userId){
+    public QuestionStatus findQuestionStatus(@PathVariable String questionId, @PathVariable String userId) {
         QuestionStatus questionStatus = questionStatusRepository.findByQuestionIdAndUserId(questionId, userId);
-        if (questionStatus == null){
+        if (questionStatus == null) {
             questionStatus = new QuestionStatus();
             questionStatus.setQuestionId(questionId);
             questionStatus.setUserId(userId);
@@ -34,10 +38,10 @@ public class QuestionStatusController {
     }
 
     @PutMapping("statusUpdate/{questionId}/{userId}")
-    public QuestionStatus updateQuestionStatus(@RequestBody QuestionStatus newQuestionStatus, @PathVariable String questionId, @PathVariable String userId){
+    public QuestionStatus updateQuestionStatus(@RequestBody QuestionStatus newQuestionStatus, @PathVariable String questionId, @PathVariable String userId) {
         QuestionStatus questionStatus = questionStatusRepository.findByQuestionIdAndUserId(questionId, userId);
 
-        if(questionStatus==null){
+        if (questionStatus == null) {
             throw new QuestionStatusNotFoundException(questionId, userId);
         }
 
@@ -47,10 +51,10 @@ public class QuestionStatusController {
     }
 
     @PutMapping("noteUpdate/{questionId}/{userId}")
-    public QuestionStatus updateNote(@RequestBody QuestionStatus newQuestionStatus, @PathVariable String questionId, @PathVariable String userId){
+    public QuestionStatus updateNote(@RequestBody QuestionStatus newQuestionStatus, @PathVariable String questionId, @PathVariable String userId) {
         QuestionStatus questionStatus = questionStatusRepository.findByQuestionIdAndUserId(questionId, userId);
 
-        if(questionStatus==null){
+        if (questionStatus == null) {
             throw new QuestionStatusNotFoundException(questionId, userId);
         }
 
@@ -58,4 +62,14 @@ public class QuestionStatusController {
         questionStatusRepository.save(questionStatus);
         return questionStatus;
     }
+
+    @DeleteMapping("/delete/{questionId}")
+    public String deleteQuestionStatus(@PathVariable("questionId") String questionId) {
+        List<QuestionStatus> questionStatus = questionStatusRepository.findByQuestionId(questionId);
+        if (questionStatus != null) {
+            questionStatusRepository.deleteAll(questionStatus);
+        }
+        return "Data deleted";
+    }
+
 }
--- src/main/java/com/non/repository/QuestionStatusRepository.java
+++ src/main/java/com/non/repository/QuestionStatusRepository.java
@@ -4,6 +4,11 @@ import com.non.model.QuestionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface QuestionStatusRepository extends CrudRepository<QuestionStatus, Long> {
     QuestionStatus findByQuestionIdAndUserId(String questionId, String userId);
+
+    List <QuestionStatus> findByQuestionId(String questionId);
+
 }
